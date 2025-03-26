5bcf704406c03fa472d587ea23cffb9474be1676
OksanaNova <o.nova0804@gmail.com>
2025-03-26 10:43:11 -0600

    styles added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-
+import back from '../assets/back.png';
+import addButton from '../assets/addToList.png';
 
 function MovieDetails() {
 
@@ -24,31 +25,42 @@ function MovieDetails() {
     if(!myMovieDetails) return <p>Loading..</p>
 
     return (
-        <div className='movie-details'>
 
-            <div>
-                <img src={`https://image.tmdb.org/t/p/w500${myMovieDetails.poster_path}`} alt={myMovieDetails.title} />
-            </div>
+        <div className='movie-details-container'>
 
-            <div>
-                <h1>{myMovieDetails.title}</h1>
+            <button className='back'>
+                <img src={back} alt='back-arrow'/>
+            </button>
 
-                <div>
-                    <p>{myMovieDetails.release_date}</p>
-                    <p>{myMovieDetails.genres.map((item) => item.name).join(", ")}</p>
-                    <p>⭐{myMovieDetails.vote_average.toFixed(1)}</p>
+            <div className='movie-details'>
+
+                <div className='movie-details-image'>
+                    <img src={`https://image.tmdb.org/t/p/w500${myMovieDetails.poster_path}`} alt={myMovieDetails.title} />
                 </div>
 
-                <div>
-                    <h2>Plot</h2>
-                    <p>{myMovieDetails.overview}</p>
-                    <p><b>Made in:</b> {myMovieDetails.production_countries.map(item => item.name).join(", ")}</p>
+                <div className='movie-details-info'>
+                    <h1>{myMovieDetails.title}</h1>
 
+                    <div className='movie-details-about'>
+                        <p className='release-date'>{myMovieDetails.release_date.split("-")[0]}</p>
+                        <p className='genres'>{myMovieDetails.genres.map((item) => item.name).join(", ")}</p>
+                        <p className='vote'>⭐{myMovieDetails.vote_average.toFixed(1)}</p>
+                    </div>
+
+                    <div className='description'>
+                        <h2>Plot</h2>
+                        <p>{myMovieDetails.overview}</p>
+                        <h2>Made in</h2>
+                        <p>{myMovieDetails.production_countries.map(item => item.name).join(", ")}</p>
+
+                    </div>
                 </div>
-
-
-
             </div>
+
+            <button className='addToList'>
+                <img src={addButton} alt='add-button'/>
+            </button>
+
         </div>
     )
 }
