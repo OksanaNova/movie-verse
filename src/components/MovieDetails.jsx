39d122ffcdf676b775b5abd1414733d7c5c4829a
OksanaNova <o.nova0804@gmail.com>
2025-03-27 10:14:10 -0600

    Loader added

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import back from '../assets/back.png';
 import addButton from '../assets/addToList.png';
+import LoaderPage from './LoaderPage';
+import Swal from 'sweetalert2';
 
 function MovieDetails() {
 
@@ -12,6 +14,8 @@ function MovieDetails() {
 
     const [myMovieDetails, setMyMovieDetails] = useState(null);
 
+    const [stateLoader, setStateLoader] = useState(false);
+
     const [expanded, setExpanded] = useState(false);
 
     const location = useLocation();
@@ -21,10 +25,22 @@ function MovieDetails() {
         if(!id) return;
 
         const getMovieDetails = async () => {
+            setStateLoader(true);
             const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${MY_KEY}`);
-            const data = await response.json();
-            setMyMovieDetails(data)
-            // console.log(data)
+
+            if(response.ok) {
+                setStateLoader(false);
+                const data = await response.json();
+                setMyMovieDetails(data)
+                // console.log(data)
+            } else {
+                setStateLoader(false);
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: "Something went wrong!",
+                  });
+            }
         }
         getMovieDetails()
     }, [id])
@@ -33,53 +49,60 @@ function MovieDetails() {
         setExpanded(!expanded)
     }
 
-    if(!myMovieDetails) return <p>Loading..</p>
+    if(stateLoader) return <LoaderPage />
+
+    if(!myMovieDetails) return <p className='no-results'>Movie not found.</p>
 
     return (
 
-        <div className='movie-details-container'>
+        <>
 
-            <button onClick={() => navigate("/", { state: {searchResults, userSearch} })} className='back'>
-                <img src={back} alt='back-arrow'/>
-            </button>
+            <div className='movie-details-container'>
 
-            <div className='movie-details'>
+                <button onClick={() => navigate("/", { state: {searchResults, userSearch} })} className='back'>
+                    <img src={back} alt='back-arrow'/>
+                </button>
 
-                <div className='movie-details-image'>
-                    <img src={`https://image.tmdb.org/t/p/w500${myMovieDetails.poster_path}`} alt={myMovieDetails.title} />
-                </div>
+                <div className='movie-details'>
 
-                <div className='movie-details-info'>
-                    <h1>{myMovieDetails.title}</h1>
-
-                    <div className='movie-details-about'>
-                        <p className='release-date'>{myMovieDetails.release_date.split("-")[0]}</p>
-                        <p className='genres'>{myMovieDetails.genres.map((item) => item.name).join(", ")}</p>
-                        <p className='vote'>⭐{myMovieDetails.vote_average.toFixed(1)}</p>
+                    <div className='movie-details-image'>
+                        <img src={`https://image.tmdb.org/t/p/w500${myMovieDetails.poster_path}`} alt={myMovieDetails.title} />
                     </div>
 
-                    <div className='description'>
-                        <h2>Plot</h2>
+                    <div className='movie-details-info'>
+                        <h1>{myMovieDetails.title}</h1>
 
-                        <p>
-                            {expanded ? myMovieDetails.overview : `${myMovieDetails.overview.slice(0, 140)}... `}
-                            <button onClick={toggleOverview} className='toggle-button'>
-                                {expanded ? "Show less" : "Show more"}
-                            </button>
-                        </p>
+                        <div className='movie-details-about'>
+                            <p className='release-date'>{myMovieDetails.release_date.split("-")[0]}</p>
+                            <p className='genres'>{myMovieDetails.genres.map((item) => item.name).join(", ")}</p>
+                            <p className='vote'>⭐{myMovieDetails.vote_average.toFixed(1)}</p>
+                        </div>
 
-                        <h2>Made in</h2>
-                        <p>{myMovieDetails.production_countries.map(item => item.name).join(", ")}</p>
+                        <div className='description'>
+                            <h2>Plot</h2>
 
+                            <p>
+                                {expanded ? myMovieDetails.overview : `${myMovieDetails.overview.slice(0, 150)}... `}
+                                <button onClick={toggleOverview} className='toggle-button'>
+                                    {expanded ? "Show less" : "Show more"}
+                                </button>
+                            </p>
+
+                            <h2>Made in</h2>
+                            <p>{myMovieDetails.production_countries.map(item => item.name).join(", ")}</p>
+
+                        </div>
                     </div>
                 </div>
+
+                <button className='addToList'>
+                    <img src={addButton} alt='add-button'/>
+                </button>
+
             </div>
 
-            <button className='addToList'>
-                <img src={addButton} alt='add-button'/>
-            </button>
-
-        </div>
+        </>
+    
     )
 }
 
