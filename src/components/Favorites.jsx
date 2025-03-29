1fdeeacdbe330fb08c64bde18ff209d6e262128b
OksanaNova <o.nova0804@gmail.com>
2025-03-29 16:38:35 -0600

    favorites.jsx added

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
 
 function Favorites() {
+
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        const savedMovies = JSON.parse(localStorage.getItem('favorite')) || [];
+        setFavorites(savedMovies);
+    }, [])
+
+
+
     return (
-        <div>
-            <p>Favorites</p>
-        </div>
+        <>
+            <div>
+                <p className="carousel-header"><b>Your</b> Favorites</p>    
+                <button>Delete All</button>
+            </div>
+
+            <div className="search-result-container">
+                {favorites.length > 0 ? (
+                    favorites.map((element) => (
+                        <MovieCard
+                        key={element.id}
+                        id={element.id}
+                        title={element.title}
+                        vote={element.vote_average}
+                        poster_path={element.poster_path}/>
+                    ))
+                )
+            : (<p className="no-results">Nothing here yet!<br/> Start building your watchlist 🎬</p>)}
+
+            </div>
+
+        </>
+
+
     )
 }
 
